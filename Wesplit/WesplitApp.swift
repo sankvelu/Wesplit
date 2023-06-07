b8fadb60081190a6cde7a73098eb2928dbcbe2c3
sankara velayutham <sankaravelayutham@yahoo.com>
2023-06-07 11:09:21 +0530

    Initial Commit

---
+++ Wesplit/WesplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WesplitApp.swift
+//  Wesplit
+//
+//  Created by sankara velayutham on 07/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WesplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
